c48eb2ca324d082be32a4c00dfd27b2e3111e9e5
Solomon Adola <77428753+solomonadola@users.noreply.github.com>
2023-11-23 13:38:16 +0300

    production commit

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,16 +1,18 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: 'mern-estate.firebaseapp.com',
-  projectId: 'mern-estate',
-  storageBucket: 'mern-estate.appspot.com',
-  messagingSenderId: '1078482850952',
-  appId: '1:1078482850952:web:28f19139ab77246602fb3d',
+  authDomain: "centralhousemarketplace.firebaseapp.com",
+  projectId: "centralhousemarketplace",
+  storageBucket: "centralhousemarketplace.appspot.com",
+  messagingSenderId: "577830209078",
+  appId: "1:577830209078:web:66c2d41f7fcee22baefa97",
+  measurementId: "G-4EEDXHT6R0",
 };
 
 // Initialize Firebase
